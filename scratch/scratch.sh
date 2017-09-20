e51b9a13b48e41062addc10c7452323e716eae51
Ralph deGennaro <rdegennaro@sscps.org>
2017-09-20 09:30:02 -0400

    Delete scratch.sh

--- scratch/scratch.sh
+++
@@ -1,4 +0,0 @@
-# below checks that GAM is working, it will also prompt for upgrade if one in is available
-#../bin/gam/gam info domain
-# below downloads new version & runs install.  if the project is already setup, do not run that part when asked
-#bash <(curl -s -S -L https://git.io/install-gam)
