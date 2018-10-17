d3b8eb1be06fdbea94d605afe66bc9e498bd248f
Joe McSheffrey <jmcsheffrey@sscps.org>
2018-10-17 13:54:26 -0400

    forked, updated aduser.ps1

--- tools/aduser.ps1
+++ tools/aduser.ps1
@@ -1,8 +1,8 @@
 ########################################################################################################
-# Purpose:  Create or update AD User & profile folders based on passed parameters
+# Purpose:  Create, update or fix AD User & profile folders based on passed parameters
 #
 # Arguments, all but grade required:
-#   - cmd_type; choices: ADD|UPDATE|ARCHIVE, anything only displays info
+#   - cmd_type; choices: ADD|UPDATE|ARCHIVE|FIX, anything only displays info
 #   - unique_id
 #   - population, choices: STU|EMP
 #   - school_email
@@ -13,7 +13,7 @@
 #   - grade
 #
 # Example invocation:
-#    .\aduser.ps1 -cmd_type "ADD" -unique_id "TST99999" -population "STU" -school_email "some_dude@student.sscps.org" -user_name "some_dude" -first_name "S'ome" -last_name "Dude" -description "SSCPS Test ID" -fileserver "ROWLEY" -grade "06"
+#    .\aduser.ps1 -cmd_type "ADD" -unique_id "TST99999" -population "STU" -school_email "some_dude@student.sscps.org" -user_name "some_dude" -first_name "Some" -last_name "Dude" -description "SSCPS Test ID" -fileserver "ROWLEY" -grade "06"
 #
 # REQUIRES:  subinacl tool must be installed on server you run this from
 #            https://www.microsoft.com/en-us/download/details.aspx?id=23510
@@ -71,6 +71,7 @@ $default_OU_student_level4 = "OU=Level 4"
 $default_OU_student_levelhs = "OU=Level HS"
 $default_OUPath_employee_archive = "OU=Employees,OU=Aging,OU=Archive,OU=SSCPS,DC=ad,DC=sscps,DC=org"
 $default_OUPath_student_archive = "OU=Students,OU=Aging,OU=Archive,OU=SSCPS,DC=ad,DC=sscps,DC=org"
+$logfile = "C:\TechTools\bin\bip\logs\aduser.log"
 
 ###############################
 #
@@ -99,7 +100,6 @@ else {
    $stu_user_folder = "\\" + $fileserver + "\c$\Storage\StudentUserFiles\"
    $stu_other_folder = "\\" + $fileserver + "\c$\Storage\StudentOtherFiles\"
 }
-
 
 # finish variables based on population
 if ( $population -eq "EMP" ) {
@@ -131,6 +131,11 @@ if ( $population -eq "STU" ) {
 #
 ###############################
 
+Function Log {
+   Param ([string]$logstring)
+   Add-content $Logfile -value $logstring
+}
+
 # Fuction to create folders if needed
 # Accepts UNC (e.g. \\GREG\c$\Storage\FacStaffUserFiles\jmcsheffrey\)
 function check_user_folders($f) {
@@ -145,8 +150,9 @@ function check_user_folders($f) {
 # Function to set up permissions on folder
 # Accepts a UNC (e.g. \\GREG\c$\Storage\FacStaffUserFiles\jmcsheffrey\)
 function process_permissions($f) {
-   Write-Host "top of process_permissions"
    $u = $f | Split-Path -leaf
+   Log "process_permissions() f = $f"
+   Log "process_permissions() u = $u"
    # subinacl /subdirectories does NOT modify the root folder of path given, so we have to do it manually
    subinacl /file $f /setowner=AD\Administrator
    # Set users content owner to AD\Administrator
@@ -159,9 +165,7 @@ function process_permissions($f) {
    subinacl /file $f /setowner=$u
    # Now set the owner for all the users content back to user
    subinacl /subdirectories $f /setowner=$u
-   Write-Host "end of process_permissions"
 }
-
 
 ###############################
 #
@@ -170,41 +174,41 @@ function process_permissions($f) {
 ###############################
 
 # echo variables for varification when debugging
-Write-Host "---------------------"
-Write-Host "Passed Variables"
-Write-Host "---------------------"
-Write-Host "cmd_type:            "$cmd_type
-Write-Host "unique_id:           "$unique_id
-Write-Host "population:          "$population
-Write-Host "school_email:        "$school_email
-Write-Host "user_name:           "$user_name
-Write-Host "first_name:          "$first_name
-Write-Host "last_name:           "$last_name
-Write-Host "full_name:           "$full_name
-Write-Host "description:         "$description
-Write-Host "fileserver:          "$fileserver
-Write-Host "grade:               "$grade
-Write-Host "---------------------"
-Write-Host "Internal Variables"
-Write-Host "---------------------"
-Write-Host "password:            "$default_password
-Write-Host "enabled:             "$default_enabled
-Write-Host "homedrive:           "$default_homedrive
-Write-Host "homepath:            "$homepath
-Write-Host "user_path:           "$user_path
-Write-Host "other_path:          "$other_path
-Write-Host "organizational_unit: "$organizational_unit
-Write-Host "user_object:         "$user_object
+Log "---------------------"
+Log "Passed Variables"
+Log "---------------------"
+Log "cmd_type:            "$cmd_type
+Log "unique_id:           "$unique_id
+Log "population:          "$population
+Log "school_email:        "$school_email
+Log "user_name:           "$user_name
+Log "first_name:          "$first_name
+Log "last_name:           "$last_name
+Log "full_name:           "$full_name
+Log "description:         "$description
+Log "fileserver:          "$fileserver
+Log "grade:               "$grade
+Log "---------------------"
+Log "Internal Variables"
+Log "---------------------"
+Log "password:            "$default_password
+Log "enabled:             "$default_enabled
+Log "homedrive:           "$default_homedrive
+Log "homepath:            "$homepath
+Log "user_path:           "$user_path
+Log "other_path:          "$other_path
+Log "organizational_unit: "$organizational_unit
+Log "user_object:         "$user_object
 
 # create/update users
 if ( $cmd_type -eq "ADD" ) {
   # create active directory object
-  Write-Host "---------------------"
-  Write-Host "creating new AD User:  "$user_name
-  Write-Host "---------------------"
+  Log "---------------------"
+  Log "creating new AD User:  "$user_name
+  Log "---------------------"
   New-ADUser -Name $full_name -AccountPassword $secure_password -ChangePasswordAtLogon $change_password -Description $description -DisplayName $full_name -EmailAddress $school_email -EmployeeID $unique_id -Enabled $default_enabled -GivenName $first_name -HomeDirectory $homepath -HomeDrive $default_homedrive -Path $organizational_unit -SamAccountName $user_name -Surname $last_name -UserPrincipalName $school_email
   # create home folders & set permissions
-  Write-Host "creating home folders for user:  "$user_name
+  Log "creating home folders for user:  "$user_name
   check_user_folders($user_path)
   process_permissions($user_path)
   if( ! ( Test-Path $other_path) ) {
@@ -213,16 +217,16 @@ if ( $cmd_type -eq "ADD" ) {
   process_permissions($other_path)
 } elseif ( $cmd_type -eq "UPDATE" ) {
   # update active directory object
-  Write-Host "---------------------"
-  Write-Host "updating existing AD User:  "$user_name
-  Write-Host "---------------------"
+  Log "---------------------"
+  Log "updating existing AD User:  "$user_name
+  Log "---------------------"
   Set-ADUser -Identity $user_name -Description $description -DisplayName $full_name -EmailAddress $school_email -EmployeeID $unique_id -Enabled $default_enabled -GivenName $first_name -HomeDirectory $homepath -HomeDrive $default_homedrive -SamAccountName $user_name -Surname $last_name -UserPrincipalName $school_email
   Move-ADObject -Identity $user_object -TargetPath $organizational_unit
   Rename-ADObject -Identity $user_object -NewName $full_name
   # because powershell/ad is weird and I'm lazy - Rio
   $user_object = Get-ADuser -Identity $user_name
   # create home folders & set permissions
-  Write-Host "checking/updating/fixing permissions on home folders for user:  "$user_name
+  Log "checking/updating/fixing permissions on home folders for user:  "$user_name
   check_user_folders($user_path)
   process_permissions($user_path)
   if( ! ( Test-Path $other_path) ) {
@@ -230,26 +234,37 @@ if ( $cmd_type -eq "ADD" ) {
   }
   process_permissions($other_path)
 } elseif ( $cmd_type -eq "ARCHIVE" ) {
-  Write-Host "Disabling user:  "$user_name
+  Log "Disabling user:  "$user_name
   Set-ADUser -Identity $user_name -Enabled $FALSE
   # not sure want to remove user from all groups because of sharing in Google Drive
   #Get-ADPrincipalGroupMembership -Identity $user_name| where {$_.Name -notlike "Domain Users"} |% {Remove-ADPrincipalGroupMembership -Identity $user_name -MemberOf $_ -Confirm:$FALSE}
   if ( $population -eq "EMP" ) {
     $message = "Moving user: "+ $user_name + " to OU:  " + $default_OUPath_employee_archive
-    Write-Host $message
+    Log $message
     Move-ADObject -Identity $user_object -TargetPath $default_OUPath_employee_archive
   } elseif ( $population -eq "STU" ) {
     $message = "Moving user: "+ $user_name + " to OU:  " + $default_OUPath_student_archive
-    Write-Host $message
+    Log $message
     Move-ADObject -Identity $user_object -TargetPath $default_OUPath_student_archive
   } else {
-    Write-Host "---------------------"
-    Write-Host "Invalid population during archive."
-    Write-Host "---------------------"
+    Log "---------------------"
+    Log "Invalid population during archive."
+    Log "---------------------"
   }
+} elseif ( $cmd_type -eq "FIX" ) {
+  Log "FIX: user_name = $user_name"
+  Log "FIX: user_object = $user_object"
+  # We don't want to create folders if the AD user doesn't exist or is invalid
+  if ( ! ($user_object) ) {
+    Log "FIX: No or invalid AD object, aborting"
+    Exit
+  }
+  Log "FIX: Fixing folders for $user_name"
+  check_user_folders($user_path)
+  process_permissions($user_path)
 } else {
   # invalid cmd_type
-  Write-Host "---------------------"
-  Write-Host "Invalid command type."
-  Write-Host "---------------------"
+  Log "---------------------"
+  Log "Invalid command type."
+  Log "---------------------"
 }
